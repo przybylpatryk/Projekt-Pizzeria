f436fc165be468f901dbc1ea53d0f2dec4cefbcf
przybylpatryk <144985546+przybylpatryk@users.noreply.github.com>
2025-03-03 20:36:02 +0100

    the backbone

---
+++ Pizzeria/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Pizzeria
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                //menu
+            }
+        }
+    }
+}
